7d86d946da625b32c700b2b9acd0fccccec05f9c
WOLFSMIND\WolfsMind <cameronch1993@gmail.com>
2021-08-09 20:26:37 +1000

    updated raycasting origin and direction for better detection added testing element to scene: footpoint

--- Dev/GhostVR/Assets/GhostAssets/Scripts/AvatarRaycastController.cs
+++ Dev/GhostVR/Assets/GhostAssets/Scripts/AvatarRaycastController.cs
@@ -10,12 +10,13 @@ public class AvatarRaycastController : MonoBehaviour
     private GameObject head;
     private GameObject MainCam;
 
+    private float CameraVerticalOffset;
+    private Vector3 CameraOffset;
+
     public float ProjectionDistance;
     private Vector3 ProjectionVector;
     private float CollisionDistance;
     private Vector3 CollisionVector;
-    //private Vector3 AvatarTargetPosition;
-
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +25,13 @@ public class AvatarRaycastController : MonoBehaviour
         rig = GameObject.FindGameObjectWithTag("XRRig").GetComponent<XRRig>();
         //MainCamera
         MainCam = GameObject.Find("Main Camera").gameObject;
+
+        //Disable/enable correct camera scripts
+        GameObject.FindGameObjectWithTag("XRRig").GetComponent<CameraController>().enabled = false;
+
+        //Store Camera offset for raycasting
+        CameraVerticalOffset = MainCam.transform.position.y;
+        CameraOffset = new Vector3(0.0f, -CameraVerticalOffset, 0.0f);
     }
 
     // Update is called once per frame
@@ -31,20 +39,32 @@ public class AvatarRaycastController : MonoBehaviour
     {
         CalculateProjection();
         CalculateCollision();
+        CalculatePosition();
+        CalculateRotation();
+    }
 
+    void CalculateRotation()
+    {
+        float yRotation = MainCam.transform.rotation.eulerAngles.y;
+        this.transform.rotation = Quaternion.Euler(0.0f, yRotation, 0.0f);
+    }
+
+    void CalculatePosition()
+    {
 
 
         //IE a collision has occured
         if (CollisionDistance < ProjectionDistance)
         {
-            this.transform.position = rig.transform.position + CollisionVector;
+            this.transform.position = rig.transform.position + CollisionVector + CameraOffset;
         }
         else
         {
-            this.transform.position = rig.transform.position + ProjectionVector;
+            this.transform.position = rig.transform.position + ProjectionVector + CameraOffset;
         }
-    }
 
+        this.transform.position = new Vector3(this.transform.position.x, 0.0f, this.transform.position.z);
+    }
 
     void CalculateProjection()
     {
@@ -57,15 +77,23 @@ public class AvatarRaycastController : MonoBehaviour
         CollisionDistance = ProjectionDistance;
         CollisionVector = new Vector3(0.0f, 0.0f, ProjectionDistance);
 
+        //Define more points for higher precision checking
+        Vector3 FootPoint = MainCam.transform.position + CameraOffset + new Vector3(0.0f, 0.1f, 0.0f);
+        //GameObject.Find("Footpoint").transform.position = FootPoint; //testing
+        Vector3 FootCast = new Vector3(FootPoint.x, 0.1f, FootPoint.z);
+
+        //Remove vertical rotated portion of ray cast so is only 2d
+        Vector3 CastForward = new Vector3(MainCam.transform.forward.x, 0.0f, MainCam.transform.forward.z);
+
         RaycastHit AvatarHit;
-        if (Physics.Raycast(MainCam.transform.position, MainCam.transform.forward, out AvatarHit, ProjectionDistance))
+        if (Physics.Raycast(FootCast, CastForward, out AvatarHit, ProjectionDistance))
         {
             
             GameObject CollisionObj = AvatarHit.transform.gameObject;
             if (CollisionObj.CompareTag("Environment"))
             {
                 Debug.Log("Ray Hit");
-                CollisionVector = AvatarHit.point - MainCam.transform.position ; //May need to be more careful/precision here on collision position to account for avatar dimensions
+                CollisionVector = AvatarHit.point - MainCam.transform.position - CameraOffset; //May need to be more careful/precision here on collision position to account for avatar dimensions
             }
             else
             {
